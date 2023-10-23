dd02e3c7f13329d1fa9d286d95aa1bc5de726409
sundonghwan <sunehd564@gmail.com>
2023-10-24 00:41:31 +0900

    [Modify] Depends 기능 활용, reponse_model 적용
    author: 선동환
    date: 2023-10-24

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -3,15 +3,15 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from model import Question
-
+from domain.question import question_schema, question_crud
 # question router 정의 Author: 선동환 modify: 2023-10-23
 router = APIRouter(
     prefix="/api/question"
 )
 
 # question 전체 목록
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    _question_list = question_crud.get_question_list(db)
 
     return _question_list
