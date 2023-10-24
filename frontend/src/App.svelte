2454c1971e6825d54861a14bf2b1607cec275862
sundonghwan <sunehd564@gmail.com>
2023-10-24 22:10:04 +0900

    [Feature] 전체 질문 목록 페이지 작성
    author: 선동환
    date: 2023-10-24

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,17 +1,11 @@
 <script>
-  let question_list = [];
-  function get_question_list() {
-    fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
-      response.json().then((json) => {
-        question_list = json;
-      });
-    });
-  }
-  get_question_list();
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  import Detail from "./routes/Detail.svelte";
+  const routes = {
+    "/": Home,
+    "/detail/:question_id": Detail,
+  };
 </script>
 
-<ul>
-  {#each question_list as question}
-    <li>{question.subject}</li>
-  {/each}
-</ul>
+<Router {routes} />
---
+++ frontend/src/routes/Detail.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let params = {};
+    let question_id = params.question_id;
+    console.log("question_id:" + question_id);
+</script>
+
+<h1>제목</h1>
+<div>내용</div>
